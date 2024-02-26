59a242b066c5f899a7a956f08efc98f3ff63a77b
MukterHossain <mukterhossain3075@gmail.com>
2024-02-26 17:32:31 +0600

    added task section

--- task.js
+++ task.js
@@ -101,22 +101,118 @@
 
 
 
-// scope
+// // scope
 
-function scop(a, b){
-    const total = a + b;
-    console.log(a, b)
-    if(a>10){
-        const sum = 10 + a+ b;
-        console.log(sum)
-    }
+// function scop(a, b){
+//     const total = a + b;
+//     console.log(a, b)
+//     if(a>10){
+//         const sum = 10 + a+ b;
+//         console.log(sum)
+//     }
     
-    else{
-        const sum = 20 + a + b;
-        const current = sum;
-        console.log(current)
-    }
+//     else{
+//         const sum = 20 + a + b;
+//         const current = sum;
+//         console.log(current)
+//     }
     
-    return total;
+//     return total;
+// }
+// scop(5, 10)
+
+
+
+// Closure
+
+// function food(){
+//     let mug = 0;
+//     return function(){
+//         mug ++ ;
+//         return mug;
+//     }
+// }
+
+// const eatFood = food();
+// const eatFood1 = food();
+// console.log(eatFood())
+// console.log(eatFood1())
+
+// console.log(eatFood1())
+// console.log(eatFood1())
+// console.log(eatFood1())
+// console.log(eatFood1())
+// console.log(eatFood1())
+// console.log(eatFood1())
+// console.log(eatFood())
+// console.log(eatFood())
+// console.log(eatFood())
+// console.log(eatFood())
+
+
+
+
+
+// callback
+// function seeFood (person){
+//     console.log(person)
+// }
+// // const s = [45, 21, 24, 25, 32, 51]
+// const s = {name: 'abol', price: '21530', brand: 'CF9'}
+// seeFood(s.price)
+
+// function greeting(greetEvening, name){
+//     handlerControl(name)
+// }
+// function handlerControl(name){
+//     console.log('Hello Country', name)
+// }
+
+// function bolotNoOne(name){
+//     console.log('How are you', name)
+// } 
+// function sunGlass(book){
+//     console.log('Are you bolod', book)
+// }
+
+// greeting(handlerControl, 'tom ni')
+// greeting(handlerControl, 'Cue ni')
+// greeting(handlerControl, 'Tosi ni')
+// greeting(handlerControl, 'Poe ni')
+// greeting(bolotNoOne, 'See Book')
+// greeting(sunGlass, 'Shit Book')
+
+
+// function submitHandler(){
+//     console.log('submit')
+// }
+// document.getElementById('btn-submit').addEventListener('click', submitHandler)
+
+
+
+
+
+// arguments
+// function sum(a, b, c){
+//     // console.log(arguments[4])
+//     const args = [...arguments]
+//     console.log(args)
+//     const result = a + b + c;
+//     return result;
+// }
+// // console.log(arguments)
+// const total = sum(1, 2, 3, 21, 32, 54, 102, 52)
+// console.log(total)
+
+
+// passby
+let num1 = 5;
+let num2 = 7;
+function multiply(a, b){
+    a= 12;
+    const sum = a * b;
+    return sum;
 }
-scop(5, 10)
+console.log(num1)
+const total = multiply(num1, num2)
+console.log(total)
